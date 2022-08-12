a4da12d8a17f5b4558ccc28b907678a50c27c8b9
arunkumarbhattar <bhattar1@purdue.edu>
2022-08-11 20:43:04 -0400

    minor changes

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/twinturbo/Desktop/checkedc-parson
+# Install script for directory: /home/arun/CLionProjects/checkedc-parson
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/twinturbo/Desktop/checkedc-parson/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/arun/CLionProjects/checkedc-parson/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
